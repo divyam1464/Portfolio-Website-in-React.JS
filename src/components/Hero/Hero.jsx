a8e23838776dc0f342ae4c1b86ccae354453d831
Divyam Agarwal <divyam7246@gmail.com>
2025-10-05 14:36:05 +0530

    Changes Made

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src={profile_img} alt=''></img>
+      <h1>I'm Divyam Agarwal, Software Developer based in India.</h1>
+      <p><span>I am a Frontend Developer based from India , with 2 years of experience in the Technology Development.</span></p>
+      <div className='hero-action'>
+        <div className='hero-connect'>Connect with me</div>
+        <div className='hero-resume'>My Resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero

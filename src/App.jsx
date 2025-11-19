f4b8d5eb872844c655ca334e9f50b793c33b7681
Divyam Agarwal <divyam7246@gmail.com>
2025-11-19 20:24:03 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import About from './components/About/About'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import Services from './Components/Services/Services'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services />
     </div>
   )
 }
